e4dd45be9eaa80c68d98c65be7fd444b017b63f3
edilsonjr <edilsonjr8717@gmail.com>
2024-05-11 11:48:37 -0300

    add: sql

---
+++ acme-pay-card-service/src/main/java/br/com/acmepay/application/domain/models/CardDomain.java
@@ -0,0 +1,22 @@
+package br.com.acmepay.application.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardDomain {
+
+    private Long id;
+    private String flag;
+    private BigDecimal cardLimit;
+    //private AccountDomain cardAccount;
+
+    public void create(){
+        //Change the return for a Account
+    };
+}
