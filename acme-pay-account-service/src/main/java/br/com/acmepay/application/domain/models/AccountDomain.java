c55089a71aa14710253f21d3e7bb1fad5385efcf
edilsonjr <edilsonjr8717@gmail.com>
2024-05-11 10:41:08 -0300

    first commit

---
+++ acme-pay-account-service/src/main/java/br/com/acmepay/application/domain/models/AccountDomain.java
@@ -0,0 +1,75 @@
+package br.com.acmepay.application.domain.models;
+
+import br.com.acmepay.exceptions.BalanceToWithdrawException;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDomain {
+
+    private Long id;
+    private Integer number;
+    private Integer agency;
+    private BigDecimal balance;
+    private Boolean closed;
+    private List<CardDomain> cards;
+    private String customer;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private List<String> transactions = new ArrayList<>();
+
+    public void create(AccountDomain account) {
+        this.setId(account.id);
+        this.setCreatedAt(LocalDateTime.now());
+        this.setUpdatedAt(null);
+        this.setCustomer(createCustomer());
+        this.setCards(new ArrayList<>());
+        this.setBalance(BigDecimal.ZERO);
+        this.setNumber(account.number);
+        this.setAgency(account.agency);
+        this.setClosed(account.closed);
+        addLog("Account created successfully" + LocalDateTime.now());
+    }
+
+    public void toWithdraw(BigDecimal amount) throws BalanceToWithdrawException {
+        if(checkBalance(amount)){
+            this.balance.subtract(amount);
+            addLog("Withdrawl made successful" + amount.toString() + ".");
+        } else {
+            throw new BalanceToWithdrawException("error withdraw");
+        }
+    }
+
+    public void deposit(BigDecimal amount){
+        this.balance = this.balance.add(amount);
+        addLog("Deposit made successful" + amount.toString());
+    }
+
+    public void transfer(AccountDomain accountDestiny, BigDecimal amount) throws BalanceToWithdrawException {
+        this.toWithdraw(amount);
+        accountDestiny.deposit(amount);
+        addLog("Transfer completed successful. From: " + this.number
+                + " to " + accountDestiny.number);
+    }
+
+    public List<String> extract(){
+        return transactions;
+    }
+
+    private boolean checkBalance(BigDecimal amount){
+        return this.balance.compareTo(amount) >= 0;
+    }
+
+    private void addLog(String message){
+        this.transactions.add(message + LocalDateTime.now());
+    }
+
+}
